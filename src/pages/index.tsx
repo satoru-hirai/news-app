7a056175859da50dcc179bd73f1a9e79663bfd24
hirai <hirai@welldone.co.jp>
2021-02-24 00:32:15 +0900

    add weather component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,9 @@ import MainLayout from "../layouts";
 import styles from '../styles/Home.module.scss';
 import Article from '../components/article';
 import Nav from '../components/nav';
+import WeathierNews from '../components/weather-news';
+import PickupArticle from '../components/pickup-article';
+import WeatherNews from '../components/weather-news';
 
 export default function Home({props}) {
   return (
@@ -19,7 +22,11 @@ export default function Home({props}) {
       <div className={styles.blank} />
       <div className={styles.main}>
         <Article title="headlines" articles={props.topArticles} />
-      </div>
+        </div>
+        <div className={styles.aside}>
+          <WeatherNews weatherNews={props.weatherNews} />
+          <PickupArticle articles={props.pickupArticles} />
+        </div>
     </div>
     </MainLayout>
   )
@@ -33,12 +40,32 @@ export const getStaticProps = async () => {
   const topJson = await topRes.json();
   const topArticles = topJson?.articles;
 
+  const lat = 35.4122;
+  const lon = 139.4130;
+  const exclude = "hourly,munutely";
+  const weatherRes = await fetch(
+    `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=${exclude}&appid=db4e458e36523588329d5712f5aee894`
+  )
+  const weatherJson = await weatherRes.json();
+  const weatherNews = weatherJson;
+
+  const keyword = "software";
+  const sortBy = "popularity";
+  const pickupPageSize = 5;
+  const pickupres = await fetch(
+    `https://newsapi.org/v2/everything?q=${keyword}&language=jp&sortBy=${sortBy}&pageSize=${pickupPageSize}&apiKey=df301d89f4a647a485202b317c864ad4`
+  )
+  const pickupJson = await pickupres.json();
+  const pickupArticles = pickupJson?.articles;
+
   return {
     props: {
       props: {
         topArticles,
+        weatherNews,
+        pickupArticles
       },
-      revalidate: 60 * 10,
+      revalidate: 60,
     }
   }
 }
